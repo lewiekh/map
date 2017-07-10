15a87510732eda68c8f6ec1b3fc23771b0d0de35
lewiekh <lewiekh@gmail.com>
2017-07-10 13:25:00 -0700

    Fixed all errors provided in the code review

---
+++ javascript/json.js
@@ -0,0 +1,72 @@
+//JSON location data
+var initialMarkers = [{
+        title: "Jumeirah Beach",
+        lat: 25.221157,
+        lng: 55.255015,
+        locationId: "4e69bde418a83989ec28ec99",
+        ytVidId: "CLrbtDGv2-g"
+    },
+    {
+        title: "Burj Khalifa",
+        lat: 25.197197,
+        lng: 55.274376,
+        locationId: "4b94f4f8f964a5204b8934e3",
+        ytVidId: "cn7AFhVEI5o"
+    },
+    {
+        title: "Burj Al Arab",
+        lat: 25.141069,
+        lng: 55.185313,
+        locationId: "4bebf8d661aca593759c8500",
+        ytVidId: "yvUbza_QQ2c"
+    },
+    {
+        title: "The Dubai Fountain",
+        lat: 25.1960,
+        lng: 55.2771,
+        locationId: "4bd9b02de914a593816957fa",
+        ytVidId: "MoxxK-8MeWw"
+    },
+    {
+        title: "Rixos The Palm Dubai",
+        lat: 25.008462,
+        lng: 54.987946,
+        locationId: "501d9192e4b03fc922a9422a",
+        ytVidId: "wdTltCwp8cw"
+    },
+    {
+        title: "Wild Wadi Water Park",
+        lat: 25.138701,
+        lng: 55.188521,
+        locationId: "4b0587eff964a520fea722e3",
+        ytVidId: "8ZfcZtg7_Jc"
+    },
+    {
+        title: "Dubai Marina Walk",
+        lat: 25.080542,
+        lng: 55.140343,
+        locationId: "4b55699ef964a520b6e327e3",
+        ytVidId: "eGOI-IzvXac"
+    },
+    {
+        title: "Dubai Aquarium",
+        lat: 25.197642,
+        lng: 55.278710,
+        locationId: "4b602959f964a520f7d729e3",
+        ytVidId: "uMUgGaWRX1o"
+    },
+    {
+        title: "Jumeirah Golf Estates",
+        lat: 25.263233,
+        lng: 55.297219,
+        locationId: "4b0587f3f964a520eda822e3",
+        ytVidId: "lkwi10fiu9k"
+    },
+    {
+        title: "The World Islands",
+        lat: 25.227829,
+        lng: 55.165193,
+        locationId: "4e8e96d1cc21c15fa2c2b85d",
+        ytVidId: "y4D6RApfVCI"
+    }
+];
